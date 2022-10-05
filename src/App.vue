25bef52c16fa2c2ce9154e5e6894fd1d9009200d
Vinay Gupta <vgupta11423@gmail.com>
2022-10-04 19:55:59 -0500

    progress 1

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand float-left" href="/">Compliant Scout App</a>
+    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button> -->
+    <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+      <ul class="nav navbar-nav mr-auto">
+        
+        <!-- Home-->
+        <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+      </ul>
     </div>
-  </header>
+  </div>
+</nav>
+</div>
 
-  <main>
-    <TheWelcome />
-  </main>
+<div class="container mt-5">
+  <RouterView></RouterView>
+</div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style>
+  @import "./assets/base.css";
+  
+  /* body { 
+    background-color:rgb(202, 202, 202) !important
+  } */
+  </style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+      <form @submit.prevent="login">
+        <h2 class="mb-3">Login</h2>
+        <div class="input">
+          <label for="email">Email address</label>
+          <input
+            class="form-control"
+            type="text"
+            name="email"
+            placeholder="email@adress.com"
+          />
+        </div>
+        <div class="input">
+          <label for="password">Password</label>
+          <input
+            class="form-control"
+            type="password"
+            name="password"
+            placeholder="password123"
+          />
+        </div>
+        <div class="alternative-option mt-4">
+          You don't have an account? <span @click="moveToRegister">Register</span>
+        </div>
+        <button type="submit" class="mt-4 btn-pers" id="login_button">
+          Login
+        </button>
+        <div
+          class="alert alert-warning alert-dismissible fade show mt-5 d-none"
+          role="alert"
+          id="alert_1"
+        >
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+          ></button>
+        </div>
+      </form>
+    </div>
+  </template>
