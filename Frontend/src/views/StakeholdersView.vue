f404d90c6b6e9ea03dbb28cf1ab15131b69ea98c
Ken Valverde <ken.valverde@yahoo.com>
2022-11-03 19:26:50 -0500

    Connect StakeholdersView to display Stakeholders from backend. Commented out stakeholder.percentownership and stakeholder.loanInvested

--- Frontend/src/views/StakeholdersView.vue
+++ Frontend/src/views/StakeholdersView.vue
@@ -10,28 +10,28 @@
                     <th class="sticky-header">Place of Birth</th>
                     <th class="sticky-header">Address</th>
                     <th class="sticky-header">Email</th>
-                    <th class="sticky-header">Percent Ownership</th>
+                    <!-- <th class="sticky-header">Percent Ownership</th> -->
                     <th class="sticky-header">Spouse First Name</th>
                     <th class="sticky-header">Spouse Last Name</th>
-                    <th class="sticky-header">Loan Invested</th>
+                    <!-- <th class="sticky-header">Loan Invested</th> -->
                     <th class="sticky-header">Personal Funds Invested</th>
                     <th class="sticky-header">Actions</th>
                 </tr>
             </thead>
             <tbody>
                 <tr class="stakeholders" v-for="stakeholder in stakeholders" :key="stakeholder._id">
-                    <td>{{ stakeholder.firstName }}</td>
-                    <td>{{ stakeholder.lastName }}</td>
-                    <td>{{ stakeholder.title }}</td>
-                    <td>{{ stakeholder.dob }}</td>
-                    <td>{{ stakeholder.placeOfBirth }}</td>
-                    <td>{{ stakeholder.address }}</td>
-                    <td>{{ stakeholder.email }}</td>
-                    <td>{{ stakeholder.percentOwnership }}</td>
-                    <td>{{ stakeholder.spouseFirstName }}</td>
-                    <td>{{ stakeholder.spouseLastName }}</td>
-                    <td>{{ stakeholder.loanInvested }}</td>
-                    <td>{{ stakeholder.personalFundsInvested }}</td>
+                    <td>{{ stakeholder.s_firstName }}</td>
+                    <td>{{ stakeholder.s_lastName }}</td>
+                    <td>{{ stakeholder.s_title }}</td>
+                    <td>{{ stakeholder.s_dob }}</td>
+                    <td>{{ stakeholder.s_place_of_birth }}</td>
+                    <td>{{ stakeholder.s_address }}</td>
+                    <td>{{ stakeholder.s_email }}</td>
+                    <!-- <td>{{ stakeholder.percentOwnership }}</td> -->
+                    <td>{{ stakeholder.s_spouse_firstName }}</td>
+                    <td>{{ stakeholder.s_spouse_lastName }}</td>
+                    <!-- <td>{{ stakeholder.loanInvested }}</td> -->
+                    <td>{{ stakeholder.s_personal_funds_invested }}</td>
                     <td>
                         <span>
                             <router-link :to="{name: 'EditStakeholderComp'}" class="btn btn-warning btn-sm action-btn">Edit</router-link>
@@ -45,13 +45,20 @@
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
         data() {
             return {
-                stakeholders: [
-                    { firstName: 'John', lastName: 'Doe', title: 'Sr. Manager', dob: '1988/04/19', placeOfBirth: 'Texas, USA', address: '5454 River Lane', email: 'john@johndoe.com', percentOwnership: '9%', spouseFirstName: 'Jane', spouseLastName: 'Doe', loanInvested: 1000, personalFundsInvested: 500 }
-                ]
+                stakeholders: []
             }
+        },
+        created() {
+            let apiURL = 'http://localhost:8080/api/stakeholders';
+            axios.get(apiURL).then(res => {
+                this.stakeholders = res.data;
+            }).catch(error => {
+                console.log(error)
+            })
         }
     }
 </script>
